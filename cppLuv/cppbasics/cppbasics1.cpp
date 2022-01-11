6f4e0a0d6f4fe63c6ac919133a47669c9d19970d
Anant Chaudhary <anant_infinity@protonmail.com>
2022-01-11 21:51:28 +0530

    aoeioinaf

---
+++ cppLuv/cppbasics/cppbasics1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>// standalone library for all libraries which may be used in competetive programming
+
+using namespace std;
+
+int main()
+{
+    int a = 3;
+    char c = 'a';
+    double d = 4.7;
+    a = 7.7; //if try to store decimal in int var it prints integer of back i.e. 7
+    cout << a << " " << c << " " << d << endl;
+    return 0;
+}
