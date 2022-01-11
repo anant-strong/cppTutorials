af897a071946d46d2278cfdb1b79d0d5938cf92e
Anant Chaudhary <anant_infinity@protonmail.com>
2022-01-11 21:54:55 +0530

    changes

--- cppLuv/cppbasics/cppbasics1.cpp
+++ cppLuv/cppbasics/cppbasics1.cpp
@@ -7,7 +7,10 @@ int main()
     int a = 3;
     char c = 'a';
     double d = 4.7;
+    bool l = true;// it will print 1
+    l = 123; // in cpp every other thing it will take as true except 0, so it will also print true
     a = 7.7; //if try to store decimal in int var it prints integer of back i.e. 7
     cout << a << " " << c << " " << d << endl;
+    cout << l;
     return 0;
 }
