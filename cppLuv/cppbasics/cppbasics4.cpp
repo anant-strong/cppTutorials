f67c9c04b2bf8cbbd527567cae4085a006bba17d
Anant Chaudhary <anant_infinity@protonmail.com>
2022-01-18 11:45:06 +0530

    jhjhih

--- cppLuv/cppbasics/cppbasics4.cpp
+++ cppLuv/cppbasics/cppbasics4.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    
+    cout << 'a' + 1 << endl;
+    cout << 7/4 << endl;
+    cout << 7/2.0;
     return 0;
 }
+// in cpp operations in datatypes happens through presidence(preferrence)
+// preferrence order for data types is :
+// 1.double
+// 2.float 
+// 3.long long int
+// 4.long int 
+// 5.int 
+// 6.char 
