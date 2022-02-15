1bab80a2771d5f804e5edaeaf00c68d456b8f7bf
Anant Chaudhary <anant_infinity@protonmail.com>
2022-02-15 22:26:56 +0530

    some more modifications

--- cppLuv/patterns/11patterns.cpp
+++ cppLuv/patterns/11patterns.cpp
@@ -1,23 +1,23 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 
-using namespace std;
+// using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    int i = 1;
-    while(i<=n){
-        int j = 1;
-        while (j<=i){
-            cout << i + 1 -j;
-            j++;
-        }
-        cout << endl;
-        i++;
-    }
-    return 0;
-}   
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int i = 1;
+//     while(i<=n){
+//         int j = 1;
+//         while (j<=i){
+//             cout << i + 1 -j;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }   
 //this pattern will be printed
 // 5
 // 1
@@ -25,3 +25,23 @@ int main()
 // 321
 // 4321
 // 54321
+// another method 
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+    cin >> n;
+    int i = 1;
+    while(i <= n){
+        int j = 1;
+        int c = i;
+        while(j <= i){
+            cout << c;
+            c--;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
